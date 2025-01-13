f071933935c748840f24f38bb094785c18fe6129
Shawn Tan <shawnhadritan@gmail.com>
2025-01-13 22:35:35 +0800

    Refactor To Create Sub-Components

---
+++ src/components/WeatherForecast/WeatherData.jsx
@@ -0,0 +1,17 @@
+function WeatherData({day, conditions, time}) {
+  return (
+    <>
+      <h2>{day}</h2>
+      <p>
+        <span>conditions: </span>
+        {conditions}
+      </p>
+      <p>
+        <span>time: </span>
+        {time}
+      </p>
+    </>
+  );
+}
+
+export default WeatherData;
